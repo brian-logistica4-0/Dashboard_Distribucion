962021b71a3f63f5a901070bacee035798c0a4f7
brian-logistica4-0 <briannahuelceballos@gmail.com>
2026-05-11 10:17:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -187,8 +187,8 @@ tabla_clientes = (
     .reset_index()
 )
 tabla_clientes["RECHAZO_%"] = (
-    tabla_clientes["CF_FALLIDAS"] / tabla_clientes["CF"]
-) * 100
+    tabla_clientes["CF_FALLIDAS"] / tabla_clientes["CF"] * 100
+).round(2)
 
 top_clientes = tabla_clientes.sort_values("CF_FALLIDAS", ascending=False).head(10)
 
